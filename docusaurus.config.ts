fbb0f45747e3e1cf3be4f0b34c1d73cde95e2ddb
takahirohonda <thon6thon6@hotmail.com>
2024-08-21 08:12:57 +0200

    Fix config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/icon-circle.png",
 
   // Set the production url of your site here
-  url: "https://https://takahirohonda.github.io",
+  url: "https://takahirohonda.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/mydatahack-old-blog",
